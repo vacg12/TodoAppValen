d94ed2ac042d6b17963a55e1d9b50875c4ba5169
Valentina Carvajal <166059610+vacg12@users.noreply.github.com>
2024-05-03 16:05:14 -0400

    Update dom.js

--- app/dom.js
+++ app/dom.js
@@ -17,6 +17,7 @@ const validateInput = (input, validation) =>{
         btnForm.disabled = false;
       } else {
         btnForm.disabled = true;
+	alert('Recuerde no dejar un espacio al final al terminar de escribir.');
       }
 }
 
